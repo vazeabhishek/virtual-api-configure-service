b398775451446af71ac23ad51951f971446029a9
Abhishek Vaze <abhishek.vaze1@ingka.com>
2020-06-14 09:24:00 +0200

    added constraints

--- src/main/java/com/invicto/vaconfigureservice/entitiy/VirtualApi.java
+++ src/main/java/com/invicto/vaconfigureservice/entitiy/VirtualApi.java
@@ -10,22 +10,27 @@ import java.util.List;
 @Entity(name = "VIRTUAL_API")
 @Data
 @Table(name = "VIRTUAL_API",
-        indexes = {@Index(name = "primary_index", columnList = "VIRTUAL_API_ID", unique = true)})
+        indexes = {@Index(name = "primary_index", columnList = "VIRTUAL_API_ID", unique = true)},
+        uniqueConstraints =
+        @UniqueConstraint(columnNames = {"VIRTUAL_API_PATH", "PROJECT_ID"}))
 public class VirtualApi {
     @Id
     @Column(name = "VIRTUAL_API_ID")
     private String virtualApiId;
     @Column(name = "VIRTUAL_API_NAME")
     private String virtualApiName;
+    @Column(name = "VIRTUAL_API_PATH")
+    private String virtualApiPath;
     @Column(name = "REQUEST_METHOD")
     private String requestMethod;
     @ManyToOne(targetEntity = Project.class, fetch = FetchType.EAGER)
+    @JoinColumn(name = "PROJECT_ID")
     private Project project;
     @Column(name = "CREATED_BY")
     private String createdBy;
     @Column(name = "CREATED_DATE")
     private LocalDateTime createdDate;
-    @OneToMany(mappedBy = "virtualApi",fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "virtualApi", fetch = FetchType.EAGER)
     @JsonIgnore
     private List<VirtualApiSpecs> virtualApiSpecs;
 }
