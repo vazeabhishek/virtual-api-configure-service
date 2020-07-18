33c039232ac6dae2877a7ccf8da317d711e662d4
Abhishek Vaze <abhishek.vaze1@ingka.com>
2020-07-18 09:08:27 +0200

    changed msg

--- src/main/java/com/invicto/vaconfigureservice/exception/ApiAlreadyExistException.java
+++ src/main/java/com/invicto/vaconfigureservice/exception/ApiAlreadyExistException.java
@@ -2,7 +2,7 @@ package com.invicto.vaconfigureservice.exception;
 
 public class ApiAlreadyExistException extends AlreadyExistException {
 
-    private static final String MESSAGE = "API already in use, Please use different name";
+    private static final String MESSAGE = "API already in use, Please use different path, return code or method";
 
     public ApiAlreadyExistException(String identifier) {
         super(MESSAGE, identifier);
